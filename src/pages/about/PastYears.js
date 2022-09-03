12e8c4e244a6d9aee0f25d0277e109955059cd40
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-09-03 12:38:58 -0400

    UPDATES change background image update 2022 media page add 2022 entry for past year competetions

--- src/pages/about/PastYears.js
+++ src/pages/about/PastYears.js
@@ -6,6 +6,7 @@ import Footer from '../../components/Footer';
 import Description from '../../components/Description';
 import { Container } from 'reactstrap';
 import PerformanceTable from '../../components/PerformanceTable';
+import gallery from "../../util/gallery-organization.json";
 
 export default class PastYears extends Component {
 
@@ -167,6 +168,18 @@ export default class PastYears extends Component {
                 </div>
                 <br/><br/><br/>
                 <div className="container">
+                    <Description align="left" title="2022" description="
+                        The 2022 competition in Salt Lake City was a challenging yet rewarding experience. We have learned and explored different new design and construction techniques to transition from a wider to a narrower section in our tower. At the end, even though our tower could not be delivered to the competition due to unexpected shipping delays, we scored 100% in the Architecture category and won the Best Architecture Award which was a significant achievement."/>   
+                        <div className="year-section">
+                            {[...Array(gallery["comp"]["2022"]).keys()].map(x => {
+                                return (
+                                    <div key={`${"comp"}-${"2022"}-${x}`} className="mb-3 pics animation">
+                                        <img className="img-fluid" src={require(`../../images/Gallery/${"comp"}/${"2022"}/${(x + 1)}.jpg`)} alt="" />
+                                    </div>
+                                )
+                            })}
+                        </div>
+
                     <Description align="left" title="2020" description="
                         We’re very excited to compete in San Diego this March and look forward to seeing how other 
                         teams have tackled the torsional irregularities this year!"/>
