77251dc583c334761df005b21c1cd0dae7ab60f9
Ajinkya <ajinkya.bhosale85@gmail.com>
2023-01-02 22:15:07 -0500

    Finished documenting and cleaned up the Blog page feature

--- src/pages/blogs/Blog.js
+++ src/pages/blogs/Blog.js
@@ -8,62 +8,63 @@ import db from "../../firebase.js";
 
 
 const Blog = () => {
-    const { id } = useParams();
-    const [blog, setBlog] = useState(null);
+  const { id } = useParams();
+  const [blog, setBlog] = useState(null);
 
-    const getBlogs = async () => {
-      const response = db.collection("blogs");
-      const data = await response.get();
-      const items = [];
-      data.docs.forEach((item) => {
-        items.push(item.data());
-      });
-      let blog = items[0].blogs.find((blog) => blog.id === parseInt(id));
-      if (blog) {
-        setBlog(blog);
-      }
-    };
+  // Get a specific blog from Firebase with the corresponding id
+  const getBlogs = async () => {
+    const response = db.collection("blogs");
+    const data = await response.get();
+    let blogsData = data.docs[0].data().blogs;
+    let blog = blogsData.find((blog) => blog.id === parseInt(id));
+    if (blog) {
+      setBlog(blog);
+    }
+  };
 
-    useEffect(() => {
-      getBlogs();
-    }, [id]);
+  useEffect(() => {
+    getBlogs();
+  }, [id]);
 
-    return (
-      <>
-        <AppNavbar />
-        <Link
-          className="blog-goBack"
-          style={{ marginLeft: "20px" }}
-          to="/blogs"
-        >
-          <span> &#8592;</span> <span>Go Back</span>
-        </Link>
-        {blog ? (
-          <div className="blog-wrap">
-            <header>
-              <p className="blog-date">Published {blog.createdAt}</p>
-              <h1>{blog.title}</h1>
-              <div className="blog-subCategory">
-                {blog.subCategory.map((category, i) => (
-                  <div key={i}>
-                    <p className="chip">{category}</p>
-                  </div>
-                ))}
-              </div>
-            </header>
-            <img
-              src={require("../../images/Blogs/" + blog.cover)}
-              alt="cover"
-            />
-            <p className="blog-desc">{blog.description}</p>
-          </div>
-        ) : (
-          <EmptyList />
-        )}
+  return (
+    <>
+      <AppNavbar />
 
-        <Footer />
-      </>
-    );
+      {/* Back Button (Routes to the Blogs page) */}
+      <Link className="blog-goBack" style={{ marginLeft: "20px" }} to="/blogs">
+        <span> &#8592;</span> <span>Go Back</span>
+      </Link>
+
+      {/* Blog Data & Empty View */}
+      {blog ? (
+        <div className="blog-wrap">
+
+          {/* Blog Header */}
+          <header>
+            <p className="blog-date">Published {blog.createdAt}</p>
+            <h1>{blog.title}</h1>
+            <div className="blog-subCategory">
+              {blog.subCategory.map((category, i) => (
+                <div key={i}>
+                  <p className="chip">{category}</p>
+                </div>
+              ))}
+            </div>
+          </header>
+
+          {/* Blog Cover Image */}
+          <img src={require("../../images/Blogs/" + blog.cover)} alt="cover" />
+
+          {/* Blog Body */}
+          <p className="blog-desc">{blog.description}</p>
+        </div>
+      ) : (
+        <EmptyList />
+      )}
+
+      <Footer />
+    </>
+  );
 };
 
 export default Blog;
--- src/pages/blogs/Blogs.js
+++ src/pages/blogs/Blogs.js
@@ -8,73 +8,73 @@ import SearchBar from '../../components/SearchBar';
 import db from '../../firebase.js';
 
 const Blogs = () => {
-    const [blogs, setBlogs] = useState([]); // Current state of the blogs (i.e. blogs under a specific fileter)
-    const [blogList, setBlogList] = useState([]); // Initial state of the blogs
-    const [searchKey, setSearchKey] = useState('');
+  const [blogs, setBlogs] = useState([]); // Current state of the blogs (i.e. blogs under a specific fileter)
+  const [blogList, setBlogList] = useState([]); // Initial state of the blogs
+  const [searchKey, setSearchKey] = useState("");
 
-    // Fetches the blog data from firebase
-    const getBlogs = async () => {
-      const response = db.collection("blogs");
-      const data = await response.get();
-      let blogData = data.docs[0].data().blogs;
-      setBlogs(blogData)
-      setBlogList(blogData);
-    };
+  // Get the data for the blogs from firebase
+  const getBlogs = async () => {
+    const response = db.collection("blogs");
+    const data = await response.get();
+    let blogData = data.docs[0].data().blogs;
+    setBlogs(blogData);
+    setBlogList(blogData);
+  };
 
-    useEffect(() => {
-      getBlogs();
-    }, []);
+  useEffect(() => {
+    getBlogs();
+  }, []);
 
-    // Search submit
-    const handleSearchBar = (e) => {
-        e.preventDefault();
-        handleSearchResults();
-    };
+  // Search submit
+  const handleSearchBar = (e) => {
+    e.preventDefault();
+    handleSearchResults();
+  };
 
-    // Search for blog by category
-    const handleSearchResults = () => {
-        const allBlogs = blogList;
-        const filteredBlogs = allBlogs.filter((blog) =>
-            blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
-        );
-        setBlogs(filteredBlogs);
-    };
-
-    // Clear search and show all blogs
-    const handleClearSearch = () => {
-        setBlogs(blogList);
-        setSearchKey('');
-    };
-
-    return (
-      <div>
-        <AppNavbar />
-
-        {/* Page Header */}
-        <header className="blog-header">
-          <h2>Seismic Design Team</h2>
-          <h1>
-            <span>“</span> Blog <span>”</span>
-          </h1>
-          <p>
-            Learn more about our team! <br />
-          </p>
-        </header>
-
-        {/* Search Bar */}
-        <SearchBar
-          value={searchKey}
-          clearSearch={handleClearSearch}
-          formSubmit={handleSearchBar}
-          handleSearchKey={(e) => setSearchKey(e.target.value)}
-        />
-
-        {/* Blog List & Empty View */}
-        {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
-
-        <Footer />
-      </div>
+  // Search for blog by category
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
     );
+    setBlogs(filteredBlogs);
+  };
+
+  // Clear search and show all blogs
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
+
+  return (
+    <div>
+      <AppNavbar />
+
+      {/* Page Header */}
+      <header className="blog-header">
+        <h2>Seismic Design Team</h2>
+        <h1>
+          <span>“</span> Blog <span>”</span>
+        </h1>
+        <p>
+          Learn more about our team! <br />
+        </p>
+      </header>
+
+      {/* Search Bar */}
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchBar}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
+
+      {/* Blog List & Empty View */}
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Blogs;
