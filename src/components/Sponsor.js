3c7ab42a1c8b053b9df41a18a7e6afa64bf3e724
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2023-01-11 17:58:50 -0500

    base sponsor component implemented

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,4 +1,155 @@
 import React, { Component } from 'react';
+import "./Sponsor.css"
+
+const SponsorCard = ({sponsor, year, sponsorLevel}) => {
+  return (
+    <div>
+      <button
+        className="sponsorImageButton"
+        data-toggle="modal"
+        data-target={`#myModal${sponsor.name.replace(/ /g, "")}`}
+      >
+        <img
+          className={`${sponsorLevel}SponsorImage`}
+          src={require(`../images/Sponsors/${sponsor.logo}`)}
+          alt={sponsor.name}
+        />
+      </button>
+      <div
+        className="modal fade"
+        id={`myModal${sponsor.name.replace(/ /g, "")}`}
+        role="dialog"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">{sponsor.name}</h4>
+              <button type="button" className="close" data-dismiss="modal">
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>{sponsor.description}</p>
+            </div>
+            <p>
+              <a
+                className="btn btn-info"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={sponsor.link}
+              >
+                Read More
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+}
+
+
+export const SponsorComponent = ({ data }) => {
+  return (
+    <div className="w-100">
+      {data?.gold && (
+        <div className="w-100 partners goldSponsors">
+          <div>
+            <h3>Gold Sponsors</h3>
+            <div className="sponsorsList">
+              {data.gold.map((sponsor) => {
+                return (
+                  // <div>
+                  //   <button
+                  //     className="sponsorButton"
+                  //     data-toggle="modal"
+                  //     data-target={`#${data.year}${sponsor.name}`}
+                  //   >
+                  //     <img
+                  //       className="goldSponsorImage"
+                  //       src={require(`../images/Sponsors/${sponsor.logo}`)}
+                  //       alt={sponsor.name}
+                  //     />
+                  //   </button>
+                  //   <div
+                  //     className="modal fade"
+                  //     id={`#${data.year}${sponsor.name}`}
+                  //     role="dialog"
+                  //   >
+                  //     <div className="modal-dialog">
+                  //       <div className="modal-content">
+                  //         <div className="modal-header">
+                  //           <h4 className="modal-title">{sponsor.name}</h4>
+                  //           <button
+                  //             type="button"
+                  //             className="close"
+                  //             data-dismiss="modal"
+                  //           >
+                  //             &times;
+                  //           </button>
+                  //         </div>
+                  //         <div className="modal-body">
+                  //           <p>{sponsor.description}</p>
+                  //         </div>
+                  //       </div>
+                  //     </div>
+                  //   </div>
+                  // </div>
+                  <SponsorCard
+                    sponsor={sponsor}
+                    year={data.year}
+                    sponsorLevel="gold"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+      {data?.silver && (
+        <div className="partners silverSponsors">
+          <div>
+            <h3>Silver Sponsors</h3>
+            <div className="sponsorsList">
+              {data.silver.map((sponsor) => {
+                return (
+                  <SponsorCard
+                    sponsor={sponsor}
+                    year={data.year}
+                    sponsorLevel="silver"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+      {data?.bronze && (
+        <div className="partners bronzeSponsors">
+          <div>
+            <h3>Bronze Sponsors</h3>
+            <div className="sponsorsList">
+              {data.silver.map((sponsor) => {
+                return (
+                  <SponsorCard
+                    sponsor={sponsor}
+                    year={data.year}
+                    sponsorLevel="silver"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+
+
 
 export default class Sponsor extends Component {
 
