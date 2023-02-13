902bea8fe5a5401e1587e76c7098efbb1b9c74a8
Kaison <cheungsiukaison.soniacheung@gmail.com>
2023-02-13 13:44:51 -0500

    Updated RJC 2023 logo

--- src/pages/sponsors/Sponsors2023.js
+++ src/pages/sponsors/Sponsors2023.js
@@ -16,7 +16,7 @@ const data = {
   gold: [
     {
       name: "RJC Engineers",
-      logo: "RJC_2023.jpg",
+      logo: "RJC_2023_v2.png",
       description:
         "Read Jones Christoffersen Ltd. (RJC) is one of the largest engineering firms in Canada specializing in structural engineering, structural restoration, building science and parking planning services. We are a large but specialized firm, with a national staff of over 450 people in offices across Canada. We've worked on many UofT projects, including the Faculty of Law expansion, the UTSC Environmental Science and Chemistry Building and the UTM North Building Reconstruction.",
       link: "https://www.rjc.ca/",
